739076795a7505bfcb89864933792db325c6970e
Nikhil Laddha <mailtonik.123@gmail.com>
2024-02-19 19:54:16 +0530

    build push

---
+++ audio_recorder_streamlit/frontend/build/precache-manifest.aa319fbf2efba56b5c50380857529bae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f77689803e3a3e20b742ee1308bf72e2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "36dfe3b0e33e70db08e1",
+    "url": "./static/css/2.bfbf028b.chunk.css"
+  },
+  {
+    "revision": "36dfe3b0e33e70db08e1",
+    "url": "./static/js/2.109c0159.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.109c0159.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "118d9a7fba463e1ace0e",
+    "url": "./static/js/main.310f3dda.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
